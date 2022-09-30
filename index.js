2072f5820739c4978fce7b853feefc3d26ebd6a3
AdrMAr5 <amarkowski@student.agh.edu.pl>
2022-09-30 22:48:57 +0200

    .

--- index.js
+++ index.js
@@ -9,39 +9,6 @@ server.get('/', (_req, res) => {
   res.send('Hello Express!');
 });
 
-server.get('/fetch-wordpress-graphql', async (_req, res) => {
-  const query = `
-    query QueryPosts {
-      posts {
-        nodes {
-          id
-          content
-          title
-          slug
-          featuredImage {
-            node {
-              mediaDetails {
-                sizes {
-                  sourceUrl
-                  name
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `;
-
-  const response = await axios.post(process.env.GRAPHQL_API_URL, { query });
-  res.send(response.data.data.posts.nodes);
-});
-
-server.get('/envs', (req, res) => {
-  console.log('hello!! rebuild');
-  console.log(process.env);
-  res.send('Envs displayed in logs!');
-});
 
 server.listen(PORT, () => {
   console.log(`Application is listening at port ${PORT}`);
