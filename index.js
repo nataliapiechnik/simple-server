63f1cbc9d3182e22c7c22de6997a1e5b308e5626
Adrian Markowski <amarkowski@student.agh.edu.pl>
2022-11-03 17:42:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3300;
 server.use(express.static('public'));
 
 server.get('/', (_req, res) => {
-  res.send('Hello Expkldfasgdfdhdfasggdgdsmkfsdgftyifuygluhklmkjglhujlmress!');
+  res.send('Hello Expkldfasgdfdhdfasggdgdsmkfsdgftyhgcjvkblnifuygluhklmkjglhujlmress!');
 });
 
 
