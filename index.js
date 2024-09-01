55fa8954bebe4b80844e98764aed18a73018825a
Adrian Markowski <amarkowski@student.agh.edu.pl>
2024-09-02 00:54:44 +0200

    log when app gets a request

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const PORT = process.env.PORT || 3300;
 server.use(express.static('public'));
 
 server.get('/', (_req, res) => {
+  console.log('app got an request on / path');
   res.send('Hello Expkldfasgdfdhdfasgguhklmkjkjkhjnlkgvhkglhujlmress!');
+});
+server.get('/test', (_req, res) => {
+  console.log('app got an request on /test path');
+  res.send('Hello test!');
 });
 
 
