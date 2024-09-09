52ab7bac0f9ed0f8d82965837ec13836943b7af8
natalia.piechnik <natalia.piechnik@wpengine.com>
2024-09-09 13:33:25 +0200

    logs

--- index.js
+++ index.js
@@ -14,10 +14,17 @@ server.get("/test", (_req, res) => {
   res.send("Hello test!");
 });
 
-process.stderr.write("FIND-IT-TEST process error");
-
-process.stderr.write("FIND-IT-TEST process error");
-
+server.get("/", (_req, res) => {
+  process.stdout.write("Hello_" + _req.query.id);
+  res.send("Hello Express from bbapp 2!");
+  console.log("parasol" + _req.query.id);
+  console.error("some error 122###############");
+  process.stdout.write("Dzien dobry_" + _req.query.id);
+  process.stdout.write("FIND-IT-TEST process stdout");
+  process.stderr.write("FIND-IT-TEST process stdout");
+  console.log("FIND-IT-TEST console log");
+  console.error("FIND-IT-TEST console error");
+});
 server.listen(PORT, () => {
   console.log(`Application is listening at port ${PORT}`);
 });
