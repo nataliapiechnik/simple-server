515a33bdc963c8530af27c972bf15cb388fa52db
natalia.piechnik <natalia.piechnik@wpengine.com>
2024-09-09 12:38:18 +0200

    logs

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const axios = require('axios');
-const express = require('express');
+const axios = require("axios");
+const express = require("express");
 const server = express();
 const PORT = process.env.PORT || 3300;
 
-server.use(express.static('public'));
+server.use(express.static("public"));
 
-server.get('/', (_req, res) => {
-  console.log('app got an request on / path');
-  res.send('Hello Expkldfasgdfdhdfasgguhklmkjkjkhjnlkgvhkglhujlmress!');
+server.get("/", (_req, res) => {
+  console.log("app got an request on / path");
+  res.send("Hello Expkldfasgdfdhdfasgguhklmkjkjkhjnlkgvhkglhujlmress!");
 });
-server.get('/test', (_req, res) => {
-  console.log('app got an request on /test path');
-  res.send('Hello test!');
+server.get("/test", (_req, res) => {
+  console.log("app got an request on /test path");
+  res.send("Hello test!");
 });
 
+process.stderr.write("FIND-IT-TEST process error");
+
+process.stderr.write("FIND-IT-TEST process error");
 
 server.listen(PORT, () => {
   console.log(`Application is listening at port ${PORT}`);
