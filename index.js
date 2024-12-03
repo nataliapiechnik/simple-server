884df6c707253fc67a2925adc3cbe4c53f70d7d9
Adrian Markowski <amarkowski@student.agh.edu.pl>
2024-12-03 10:16:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ server.use(express.static('public'));
 
 server.get('/', (_req, res) => {
   console.log('app got an request on / path');
-  res.send('Hello Expkldfasgdfdhdffghasgguhklmkjkjkhjnlkgvhkgljkhujlmress1232!');
+  res.send('Hello Expkldfasgdfdhdffghaasgguhklmkjkjkhjnlkgvhkgljkhujlmress1232!');
 });
 server.get('/test', (_req, res) => {
   console.log('app got an request on /test path');
