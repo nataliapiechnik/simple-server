001b01352e6721d7d3ab0a36a71aea16cd44ee02
natalia.piechnik <natalia.piechnik@wpengine.com>
2025-03-17 10:00:56 +0100

    tesgt

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ server.get("/test", (_req, res) => {
 server.get("/", (_req, res) => {
   process.stdout.write("Hello_" + _req.query.id);
   res.send("Hello Express from bbapp 2!");
-  console.log("parasol" + _req.query.id);
-  console.error("some error 122###############");
-  process.stdout.write("Dzien dobry_" + _req.query.id);
   process.stdout.write("FIND-IT-TEST process stdout");
   process.stderr.write("FIND-IT-TEST process stdout");
   console.log("FIND-IT-TEST console log");
